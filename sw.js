c8a23359f7c48874f37d6354759460d11efe92c0
AngeloSegreto <76884719+AngeloSegreto@users.noreply.github.com>
2026-08-16 17:12:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fs-control-room-v4.0.23-pwa-r4-domfast-20260816";
+const CACHE_NAME = "fs-control-room-v4.0.23-pwa-r5-combined-20260816";
 const CORE = [
   "./manifest.webmanifest",
   "./version.json",
